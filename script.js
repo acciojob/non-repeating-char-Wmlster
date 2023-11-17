3cc8bb048819d1e98837a94fdaf0539e9b84661b
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-17 19:51:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function firstNonRepeatedChar(str) {
+function firstNonRepeatedChar(s) {
  // Write your code here
+
+	for(let i=0;i<s.length;i++)
+		{
+			if(i==0&&s[i]!=s[i+1])
+			{
+				return s[i];
+			}
+			else if(i==s.length-1&&s[i]!=s[i-1])
+			{
+				return s[i];
+			}
+			else if(s[i]!=s[i-1]&&s[i]!=s[i+1])
+			{
+				return s[i];
+			}  
+		}
+
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
